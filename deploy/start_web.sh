eaef65768fbf984927f2cb477cca188d3201d927
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2026-04-22 05:10:41 +0500

    s

--- deploy/start_web.sh
+++ deploy/start_web.sh
@@ -21,7 +21,7 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 exec gunicorn config.wsgi:application \
-  --bind "${GUNICORN_BIND:-0.0.0.01:8003}" \
+  --bind "${GUNICORN_BIND:-0.0.0.0:8003}" \
   --workers "${GUNICORN_WORKERS:-3}" \
   --timeout "${GUNICORN_TIMEOUT:-120}" \
   --access-logfile - \
